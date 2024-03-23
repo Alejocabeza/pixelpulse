004934e7817d306901197642007d806c01781d21
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2024-03-22 21:49:30 -0400

    feat: :sparkles: add new button component

--- packages/components/button/src/buttonProps.ts
+++ packages/components/button/src/buttonProps.ts
@@ -1,4 +1,54 @@
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary' | 'danger';
-  as?: string;
+  /**
+   * The variant
+   */
+  variant?: 'solid' | 'faded' | 'bordered' | 'light' | 'flat' | 'ghost' | 'shadow'
+  /**
+   * The size
+   */
+  size?: 'small' | 'medium' | 'large'
+  /**
+   * The fontSize
+   */
+  color: 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
+  /**
+   * The radius
+   */
+  radius?: 'full' | 'large' | 'medium' | 'small' | 'none'
+  /**
+   * The loading
+   */
+  loading?: boolean
+  /**
+   * The sx
+   */
+  sx?: {}
+  /**
+   * The as
+   */
+  as?: string
+  /**
+   * The disabled
+   */
+  disabled?: boolean
+  /**
+   * the className
+   */
+  className?: string
+  /**
+   * The icon
+   */
+  icon?: React.ReactElement
+  /**
+   * The positionIcon
+   */
+  positionIcon?: 'start' | 'end'
+  /**
+   * The spinner
+   */
+  spinner?: React.ReactElement
+  /**
+   * The isOnlyIcon
+   */
+  isOnlyIcon?: boolean
 }
